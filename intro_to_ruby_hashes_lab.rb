d6b073aa478843339d07d56bd88133a19dd520c9
Joshua Sanders <ninjosh2@gmail.com>
2020-01-04 02:36:04 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -14,6 +14,10 @@ def monopoly
 end
 
 def monopoly_with_second_tier
+<<<<<<< HEAD
+=======
+	def monopoly
+>>>>>>> 2c7e96bfb9bea6022cfd89a323a7960537fe459b
   monopoly = {
     railroads: {
       pieces: 4,
